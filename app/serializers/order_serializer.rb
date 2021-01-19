2e7c0df4a4159b0607af95507f392609fad41085
jennifer-yoo <jennifer.yoo7@gmail.com>
2021-01-18 23:11:35 -0500

    adds serializer

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,3 +1,7 @@
 class OrderSerializer < ActiveModel::Serializer
   attributes :id, :patient_id, :doctor_id, :medication_id, :confirmation_num
+
+  belongs_to :patient, include: :all
+  belongs_to :medication, include: :all
+  belongs_to :doctor, include: :all
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :first, :last, :email, :password
+
+  has_many :orders, include: :all
+  has_many :medications, include: :all
 end
